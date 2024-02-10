c5d57f9a953e6a87e3a0cc3df8d32a8bd14625f2
louohs <louisa.hadjili@epitech.eu>
2024-02-10 16:37:53 +0100

    update

---
+++ src/Components/UI/Item/Title/TitlePageNotFound.jsx
@@ -0,0 +1,17 @@
+const TitlePageNotFound = () => {
+  return (
+    <div
+      className={`bg-0d1216 flex flex-col justify-center items-center h-screen`}
+    >
+      <div className={`flex flex-col justify-center items-center`}>
+        <h1 className={`text-8b3dff font-reem text-5xl lg:text-8xl`}>404</h1>
+        <h1 className={`text-8b3dff font-reem text-5xl lg:text-8xl`}>
+          Mauvais chemin
+        </h1>
+        <div className={`flex justify-center mt-5`}></div>
+      </div>
+    </div>
+  );
+};
+
+export default TitlePageNotFound;
--- src/Page/Error/PageNotFound.jsx
+++ src/Page/Error/PageNotFound.jsx
@@ -1,5 +1,15 @@
+import Footer from "../../Components/Footer/Footer";
+import HeaderHome from "../../Components/Navigation/Header/HeaderHome";
+import TitlePageNotFound from "../../Components/UI/Item/Title/TitlePageNotFound";
+
 const PageNotFound = () => {
-  return <p></p>;
+  return (
+    <>
+      <HeaderHome />
+      <TitlePageNotFound />
+      <Footer />
+    </>
+  );
 };
 
 export default PageNotFound;
