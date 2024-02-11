fcb2afadadd50b115d378f2f63fac0f94fd6762e
louohs <louisa.hadjili@epitech.eu>
2024-02-11 16:54:40 +0100

    update carousel

--- src/Page/Projects/Carousel/MainCarousel.jsx
+++ src/Page/Projects/Carousel/MainCarousel.jsx
@@ -5,6 +5,10 @@ const slides = [
   "https://i.ibb.co/B3s7v4h/2.png",
   "https://i.ibb.co/ncrXc2V/1.png",
   "https://i.ibb.co/B3s7v4h/2.png",
+  "https://i.ibb.co/ncrXc2V/1.png",
+  "https://i.ibb.co/B3s7v4h/2.png",
+  "https://i.ibb.co/ncrXc2V/1.png",
+  "https://i.ibb.co/B3s7v4h/2.png",
 ];
 
 export default function MainCarousel() {
@@ -12,7 +16,7 @@ export default function MainCarousel() {
     <div className={`flex justify-center py-10 h-96 w-96 3xl:w-2/3`}>
       <Carousel>
         {slides.map((s) => (
-          <img src={s} />
+          <img src={s} className={`rounded-xl`} />
         ))}
       </Carousel>
     </div>
