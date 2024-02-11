8c5a81b6d2fa90fb4d6b41aa6035a1e8da9f9432
louohs <louisa.hadjili@epitech.eu>
2024-02-11 09:43:17 +0100

    update title about and project

--- src/Components/UI/Item/Title/TitleAbout.jsx
+++ src/Components/UI/Item/Title/TitleAbout.jsx
@@ -1,6 +1,6 @@
 const TitleAbout = () => {
   return (
-    <div className={`bg-0d1216 flex flex-col justify-start h-36 px-16`}>
+    <div className={`bg-0d1216 flex flex-col justify-start text-center lg:text-left h-36 px-16`}>
       <h1 className={`text-8b3dff font-reem text-3xl lg:text-5xl`}>À propos</h1>
       <p className={`text-8b3dff font-jacques text-xl lg:text-3xl`}>
         Découvrez mon parcours professionnel, ainsi que ma passion du monde
