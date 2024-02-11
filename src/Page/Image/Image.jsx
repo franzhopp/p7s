d82311aed30dd7f312e61e0f448ecfa919684df4
louohs <louisa.hadjili@epitech.eu>
2024-02-11 19:25:17 +0100

    add copyright picture

--- src/Page/Image/Image.jsx
+++ src/Page/Image/Image.jsx
@@ -1,8 +1,12 @@
+import SvgCopyright from "../../Components/UI/Item/Svg/CopyrightPicture.jsx";
+
 const Picture = () => {
   return (
     <section x-comp="Picture">
-      <div className="bg-gray-300 flex justify-center items-center h-64">
-        <h1 className="font-reem text-5xl text-gray-500">PHOTO</h1>
+      <div className="bg-gray-300 h-64">
+        <div className="flex justify-start px-2 pt-52">
+          <SvgCopyright />
+        </div>
       </div>
     </section>
   );
