45be69feca82e6758754aed6032ba2c6bce0b391
louohs <louisa.hadjili@epitech.eu>
2024-02-11 08:53:57 +0100

    add link network

--- src/Components/UI/Item/Title/TitleContact.jsx
+++ src/Components/UI/Item/Title/TitleContact.jsx
@@ -1,3 +1,5 @@
+import { NavLink } from "react-router-dom";
+
 const TitleContact = () => {
   return (
     <div className={`flex flex-col text-center h-36 px-16`}>
@@ -5,8 +7,11 @@ const TitleContact = () => {
         Contactez-moi
       </h1>
       <p className={`text-0d1216 font-jacques text-xl lg:text-3xl`}>
-        Travaillons ensemble, vous pouvez également trouver mes réseaux sociaux
-        en cliquant ici.
+        Travaillons ensemble, vous pouvez également trouver mes réseaux sociaux{" "}
+        <NavLink to="/" className={`text-8b3dff underline`}>
+          en cliquant ici
+        </NavLink>
+        .
       </p>
     </div>
   );
