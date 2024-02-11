9aec814c791b5db67564636e167d68ff4d03cec2
louohs <louisa.hadjili@epitech.eu>
2024-02-11 22:03:30 +0100

    add picture louisa

--- src/Page/Image/Image.jsx
+++ src/Page/Image/Image.jsx
@@ -1,10 +1,13 @@
 import SvgCopyright from "../../Components/UI/Item/Svg/CopyrightPicture.jsx";
+import "./Image.css";
 
 const Picture = () => {
   return (
-    <section x-comp="Picture">
-      <div className="bg-gray-300 h-64">
-        <div className="flex justify-start px-2 pt-52">
+    <section x-comp={`Picture`}>
+      <div
+        className={`louisaimage bg-cover bg-no-repeat lg:bg-left bg-center h-64`}
+      >
+        <div className={`flex justify-start px-2 pt-52`}>
           <SvgCopyright />
         </div>
       </div>
