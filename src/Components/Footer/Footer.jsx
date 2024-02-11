b0c6ed96cde5b67b1c4fd9f7becea9276096bfb9
louohs <louisa.hadjili@epitech.eu>
2024-02-11 09:28:19 +0100

    update footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,12 +1,14 @@
 const Footer = () => {
   return (
-    <footer className=" bg-8b3dff">
-      <div className="relative mx-auto max-w-screen px-4 py-16 sm:px-6 lg:px-8 lg:pt-24">
-        <div className="lg:flex lg:items-end lg:justify-between">
+    <footer className={`bg-8b3dff`}>
+      <div
+        className={`mx-auto max-w-screen py-16 px-4 sm:px-6 lg:px-8 lg:pt-24`}
+      >
+        <div className={`lg:flex lg:items-end lg:justify-between`}>
           <div>
-            <div className="flex justify-center lg:justify-start">
+            <div className={`flex justify-center lg:justify-start`}>
               <svg
-                className="cursor-pointer"
+                className={`cursor-pointer`}
                 width="60"
                 height="60"
                 viewBox="0 0 100 100"
@@ -23,43 +25,47 @@ const Footer = () => {
                 />
               </svg>
             </div>
-
-            <p className="mx-auto mt-6 max-w-md text-center leading-relaxed font-reem  text-ffffff lg:text-left">
+            <p
+              className={`text-ffffff mx-auto mt-6 max-w-md text-center leading-relaxed font-reem lg:text-left`}
+            >
               Ce portfolio a été imaginé et développé par l’auteur.
             </p>
           </div>
-
-          <ul className="mt-12 font-reem flex flex-wrap justify-center gap-6 md:gap-8 lg:mt-0 lg:justify-end lg:gap-12">
+          <ul
+            className={`mt-12 flex flex-wrap justify-center font-reem gap-6 md:gap-8 lg:mt-0 lg:justify-end lg:gap-12`}
+          >
             <li>
-              <a className="text-ffffff transition hover:opacity-80" href="#">
+              <a className={`text-ffffff transition hover:opacity-80`} href="/">
                 {" "}
                 Accueil{" "}
               </a>
             </li>
 
             <li>
-              <a className="text-ffffff  transition hover:opacity-80" href="#">
+              <a className={`text-ffffff transition hover:opacity-80`} href="/">
                 {" "}
                 À propos{" "}
               </a>
             </li>
 
             <li>
-              <a className="text-ffffff  transition hover:opacity-80" href="#">
+              <a className={`text-ffffff transition hover:opacity-80`} href="/">
                 {" "}
                 Projects{" "}
               </a>
             </li>
 
             <li>
-              <a className="text-ffffff  transition hover:opacity-80" href="#">
+              <a className={`text-ffffff transition hover:opacity-80`} href="/">
                 {" "}
                 Contact{" "}
               </a>
             </li>
           </ul>
         </div>
-        <p className="mt-12 text-center text-sm font-reem text-ffffff lg:text-right">
+        <p
+          className={`text-ffffff mt-12 text-center text-sm font-reem lg:text-right`}
+        >
           Copyright &copy; 2024. All rights reserved.
         </p>
       </div>
